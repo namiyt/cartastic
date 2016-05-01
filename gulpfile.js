3ccfefbfbda94a6ae8d427134764fed095330c15
Tim Park <timothsp@uci.edu>
2016-05-01 12:50:06 -0700

    Added .gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,7 @@ gulp.task('watch', ['browserSync'], function() {
 	gulp.watch('app/**/*.html', browserSync.reload);
 	gulp.watch('app/css/*.css', browserSync.reload);
 	gulp.watch('app/js/**/*.js', browserSync.reload);
-	gulp.watch('app/*.php', browserSync.reload);
+	gulp.watch('app/php/*.php', browserSync.reload);
 });
 
 gulp.task('default', ['watch']);
